b488b53fbb69e3fb1af7fc119e564d542d754e02
Nathan Bell <nathan.bell421@icloud.com>
2021-01-27 23:36:15 -0600

    add update and delete

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -21,3 +21,13 @@ module.exports.createProducts = (req,res) => {
         .then(product=>res.json(product))
         .catch(err=>res.json({message:"Somthing went wrong!", error: err}))
 };
+module.exports.updateProduct = (req, res) => {
+    Product.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+        .then(updateProduct => res.json({product: updateProduct}))
+        .catch(err=>res.json({message:"Somthing went wrong", error: err}));
+};
+module.exports.deleteProduct = (req, res) => {
+    Product.deleteOne({_id: req.params.id})
+        .then(deleteConfirmation => res.json({product: deleteConfirmation}))
+        .catch(err=>res.json({message:"Something went wrong", error: err}));
+};
